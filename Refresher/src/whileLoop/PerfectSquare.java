c288bc2b4a40ca9057be183de31983ad881d2bfc
Bharath Reddy <49243941+bharathrajreddy@users.noreply.github.com>
2023-07-06 06:37:57 +0530

    Staging brach created

---
+++ Refresher/src/whileLoop/PerfectSquare.java
@@ -0,0 +1,16 @@
+package whileLoop;
+public class PerfectSquare {
+    public static int solve(int A) {
+        double num = Math.sqrt(A);System.out.println(num*num);
+        if((num)%1 == 0){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        int num = 1332;
+        System.out.println(solve(num));
+    }
+}
